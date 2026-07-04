5e0bb3fc504e14bc54109bf74074bba0595564e1
Carlos-A-Y <Carlos-A-Y@users.noreply.github.com>
2026-07-04 16:05:20 +0200

    Add GMAT score level estimator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "flight-gmat-offline-v6";
+const CACHE_NAME = "flight-gmat-offline-v7";
 const APP_SHELL = ["./", "./index.html"];
 
 self.addEventListener("install", (event) => {
