8a6244e2e7908a70f34db74ee2a21b9c122a3110
Carlos-A-Y <Carlos-A-Y@users.noreply.github.com>
2026-07-04 17:27:31 +0200

    Redesign Flight GMAT interface

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "flight-gmat-offline-v10";
+const CACHE_NAME = "flight-gmat-offline-v11";
 const APP_SHELL = ["./", "./index.html"];
 
 self.addEventListener("install", (event) => {
